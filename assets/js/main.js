62b93e46c1ae77ff7e0a44dbdf8906b360100c68
Ivan <ivanstame@yahoo.com>
2020-03-26 02:13:37 -0700

    35% translate path-window

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ nextButton.addEventListener('click', e => {
 const windowPane = document.querySelector('#path-window');
 
 windowPane.addEventListener('swiped-right', e => {
-	windowPane.style.transform = 'translateX(40%)';
+	windowPane.style.transform = 'translateX(35%)';
 });
 
 windowPane.addEventListener('swiped-left', e => {
