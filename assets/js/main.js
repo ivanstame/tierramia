306eaefff884a7fcb52f86b86e039ccb080afe08
Ivan <ivanstame@yahoo.com>
2020-03-25 17:34:18 -0700

    Added swipe functionality to shop carousel!

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,42 @@ const setSlidePosition = (slide, index) => {
 	slide.style.left = slideWidth * index + 'px';
 };
 
+const addSwipeToSlides = (slide, index) => {
+	slide.addEventListener('swiped-left', e => {
+		// alert('swiped-left!');
+		const currentSlide = track.querySelector('.current_slide');
+		const nextSlide = currentSlide.nextElementSibling;
+
+		moveToSlide(track, currentSlide, nextSlide);
+		targetIndex++;
+		prevButton.style.visibility = 'visible';
+
+		if (targetIndex == slides.length - 1) {
+			nextButton.style.visibility = 'hidden';
+		} else {
+			nextButton.style.visibility = 'visible';
+		}
+	});
+	slide.addEventListener('swiped-right', e => {
+		// alert('swiped-right!');
+		const currentSlide = track.querySelector('.current_slide');
+		const previousSlide = currentSlide.previousElementSibling;
+
+		moveToSlide(track, currentSlide, previousSlide);
+
+		targetIndex--;
+		nextButton.style.visibility = 'visible';
+		if (targetIndex == 0) {
+			prevButton.style.visibility = 'hidden';
+		} else {
+			prevButton.style.visibility = 'visible';
+		}
+	});
+};
+
 // then simply plug that shit in
 slides.forEach(setSlidePosition);
-
+slides.forEach(addSwipeToSlides);
 // create a function that moves the slides to where they need to go
 // when this functions parameters are created you have to ask yourself "what is
 // needing to be affected by this function in order for it to do its job"
