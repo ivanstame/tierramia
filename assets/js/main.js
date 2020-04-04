364f7983b3cfcaebcbc1a1b4949d991986b27ca3
Ivan <ivanstame@yahoo.com>
2020-04-04 00:58:33 -0700

    Phone, tablet, desktop mq rd1

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,4 +130,13 @@ setInterval(function() {
 	}
 }, 1000);
 
+$(window).on('resize', function() {
+	var win = $(this); //this = window
+	if (win.height() >= 820) {
+		/* ... */
+	}
+	if (win.width() >= 1024) {
+	}
+});
+
 //when the window resizes we have to have the slides reposition themselves
