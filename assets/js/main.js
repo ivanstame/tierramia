edaaba699f4b1597623f08aa0f7c8bb6c90fbe06
Ivan <ivanstame@yahoo.com>
2020-04-07 16:42:52 -0700

    yet again

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 //fix vh
 let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vw', `${vw}px`);
 
 const track = document.querySelector('.slider-track');
 const slides = Array.from(track.children);
