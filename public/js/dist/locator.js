a928c2716892d9044b082021b51298ddf2f0407e
Ivan <ivanstame@yahoo.com>
2020-04-14 16:19:30 -0700

    errsdgf

--- public/js/dist/locator.js
+++ public/js/dist/locator.js
@@ -1,29 +1,5 @@
 
-  window.onload = function() {
-    navigator.geolocation.getCurrentPosition(sortResults);
-  }
 
-function sortResults(position) {
-    // Grab current position
-    var latlon = new LatLon(position.coords.latitude, position.coords.longitude);
-    return 2;
-
-    var targets = document.getElementById('locations-container').getElementsByTagName('a');
-    var targetedList = Array.from(targets);
-
-
-    targetedList.sort(function(a,b){
-      var locA  = a.getAttribute('data-latlon').split(',');
-      var locB  = b.getAttribute('data-latlon').split(',');
-
-      distA = latlon.distanceTo(new LatLon(Number(locA[0]),Number(locA[1])));
-      distB = latlon.distanceTo(new LatLon(Number(locB[0]),Number(locB[1])));
-      return distA - distB;
-    });
-
-    console.log(targetedList);
-
-}
 // window.onload = function(){
 //   navigator.geolocation.getCurrentPosition(
 //       function(position) {
